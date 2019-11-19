d1eedf12e995c9f56714eb78eae7ab9e1bb95b88
tedhchen <ted.hsuanyun.chen@gmail.com>
2019-11-19 20:30:05 +0200

    updated unicode enable for dz60rgb

--- keyboards/dztech/dz60rgb/rules.mk
+++ keyboards/dztech/dz60rgb/rules.mk
@@ -3,12 +3,18 @@ MCU = STM32F303
 
 BACKLIGHT_ENABLE = no
 BOOTMAGIC_ENABLE = lite           # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes              # Mouse keys
+MOUSEKEY_ENABLE = no              # Mouse keys
 EXTRAKEY_ENABLE = yes              # Audio control and System control
 CONSOLE_ENABLE = no                # Console for debug
 COMMAND_ENABLE = no                # Commands for debug and configuration
-#SLEEP_LED_ENABLE = yes            # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = no            # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no                 # USB Nkey Rollover
 AUDIO_ENABLE = no
 RGB_MATRIX_ENABLE = IS31FL3733     # Use RGB matrix
 NO_USB_STARTUP_CHECK = no          # Disable initialization only when usb is plugged in
+<<<<<<< Updated upstream
+=======
+UNICODEMAP_ENABLE = yes
+
+LAYOUTS = 60_ansi
+>>>>>>> Stashed changes
